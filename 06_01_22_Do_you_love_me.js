e38200ea3260f1f3f65fa6ba40446e8ea24564d7
Thiet Nguyen <nguyenthiet1225@gmail.com>
2022-01-07 05:34:16 +0700

    new pj

---
+++ 06_01_22_Do_you_love_me.js
@@ -0,0 +1,10 @@
+function love(){
+    alert("<3");
+}
+function dont(){
+    var x = Math.random() * 500;
+    var y = Math.random() * 500;
+    document.getElementById('n').style.left = x + 'px';
+    document.getElementById('n').style.top = y + 'px';
+
+}
